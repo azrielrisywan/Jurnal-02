38e500e1d160287c1a393aad8d9796da9c43d590
unknown <azrielrisywan@gmail.com>
2020-09-16 18:26:20 +0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Azriel Risywan Attaufiqi");
+	    System.out.println("Kelas=D3IF-44-01");
+        System.out.println("Nim=6706202122");
+        System.out.println("Hello Azriel!");
+    }
+}
